42c7a4d07260c8d28b8a1617afecc1d32f394f8b
Taylor Phillips <taylorphillips133@gmail.com>
2020-03-25 13:47:52 -0700

    start our debugging process

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,23 +1,12 @@
-$(document).ready(function() {
-  $("#formOne").submit(function(event) {
-    event.preventDefault();
-    var person1Input = $("input#person1").val();
-    var person2Input = $("input#person2").val();
-    var animalInput = $("input#animal").val();
-    var exclamationInput = $("input#exclamation").val();
-    var verbInput = $("input#verb").val();
-    var nounInput = $("input#noun").val();
-    
+$(document).ready(function () {
+  $("#blanks form").submit(function () {
+    var blanks = ["person1", "person2", "animal", "exclamation", "verb", "noun"];
 
-    $(".person1").text(person1Input);
-    $(".person2").text(person2Input);
-    $(".animal").text(animalInput);
-    $(".exclamation").text(exclamationInput);
-    $(".verb").text(verbInput);
-    $(".noun").text(nounInput);
+    blanks.forEach(function (blank) {
+      var userInput = $("input." + blank).val();
+      $("." + blank).text(userInput).val();
+    });
 
-    $("#story").show();
+    $("#story").sho();
   });
-
 });
-  
