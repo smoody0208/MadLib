faba8c9185bd94030e35d4a653f04f67c8f8c3d3
Taylor Phillips <taylorphillips133@gmail.com>
2020-03-25 13:20:26 -0700

    add Jquery bling where it had been left out js and now page works as intended

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
   $("#formOne").submit(function(event) {
+    event.preventDefault();
     var person1Input = $("input#person1").val();
     var person2Input = $("input#person2").val();
     var animalInput = $("input#animal").val();
@@ -7,16 +8,16 @@ $(document).ready(function() {
     var verbInput = $("input#verb").val();
     var nounInput = $("input#noun").val();
     
-    
-    (".person1").text(person1Input);
-    (".person2").text(person2Input);
-    (".animal").text(animalInput);
-    (".exclamation").text(exclamationInput);
-    (".verb").text(verbInput);
-    (".noun").text(nounInput);
+
+    $(".person1").text(person1Input);
+    $(".person2").text(person2Input);
+    $(".animal").text(animalInput);
+    $(".exclamation").text(exclamationInput);
+    $(".verb").text(verbInput);
+    $(".noun").text(nounInput);
 
     $("#story").show();
-
-    event.preventDefault();
   });
+
 });
+  
